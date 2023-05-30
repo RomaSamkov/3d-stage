255be2a9ab43ac0846debdcb1137303d9adf9de3
RomaSamkov <92638707+RomaSamkov@users.noreply.github.com>
2023-05-30 14:45:04 +0000

    Deploying to gh-pages from @ RomaSamkov/3d-stage@826fbc9a2b7e88761abb3dd7e006b33adbf29891 🚀

--- index.2974500a.js
+++
@@ -1,2 +0,0 @@
-document.addEventListener("DOMContentLoaded",function(){});
-//# sourceMappingURL=index.2974500a.js.map
---
+++ index.446c268a.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){document.querySelector("video").playbackRate=2});
+//# sourceMappingURL=index.446c268a.js.map

52194660023d10c440ecd408a85e50c077719e76
RomaSamkov <92638707+RomaSamkov@users.noreply.github.com>
2023-06-01 14:20:56 +0000

    Deploying to gh-pages from @ RomaSamkov/3d-stage@3029610c8db306e827118b10950d4e7ff1112447 🚀

--- index.3a556e4d.js
+++
@@ -1,2 +0,0 @@
-document.addEventListener("DOMContentLoaded",function(){new Swiper(".showcase-carousel",{loop:!0,slidesPerView:3,speed:1800,centeredSlides:!0}),document.querySelector("video").playbackRate=2});
-//# sourceMappingURL=index.3a556e4d.js.map
